ec72422c66580f9c1e9f3c836157ab78659a5f42
rcsn01 <chris.eves0426@gmail.com>
2026-02-09 16:34:17 +1100

    .

--- stoqr/web-app/src/components/Badge.tsx
+++
@@ -1,5 +0,0 @@
-type BadgeVariant = 'success' | 'warning' | 'danger' | 'neutral' | 'info'
-
-export const Badge = ({ label, variant }: { label: string; variant: BadgeVariant }) => {
-  return <span className={`badge-pill ${variant}`}>{label}</span>
-}
